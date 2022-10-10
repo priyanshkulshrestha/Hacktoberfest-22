736cc38ab2c19e1fe9faa183344210bb9fb7b389
Harsh Pandey <harsh.pp303@gmail.com>
2022-10-10 13:48:25 +0530

    spirally Traversing

--- dsa/array/arraySubsetOfAnotherArray.java
+++ dsa/array/arraySubsetOfAnotherArray.java
@@ -4,24 +4,21 @@
 //Initial Template for Java
 
 import java.util.*;
-import java.lang.*;
+
 import java.io.*;
 
 class GFG {
-	public static void main(String[] args) throws IOException
-	{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
-        while(t-->0)
-        {
+        while (t-- > 0) {
             StringTokenizer stt = new StringTokenizer(br.readLine());
-            
+
             long n = Long.parseLong(stt.nextToken());
             long m = Long.parseLong(stt.nextToken());
-            long a1[] = new long[(int)(n)];
-            long a2[] = new long[(int)(m)];
-            
-            
+            long a1[] = new long[(int) (n)];
+            long a2[] = new long[(int) (m)];
+
             String inputLine[] = br.readLine().trim().split(" ");
             for (int i = 0; i < n; i++) {
                 a1[i] = Long.parseLong(inputLine[i]);
@@ -30,38 +27,32 @@ class GFG {
             for (int i = 0; i < m; i++) {
                 a2[i] = Long.parseLong(inputLine1[i]);
             }
-            
-            
+
             Compute obj = new Compute();
-            System.out.println(obj.isSubset( a1, a2, n, m));
-            
+            System.out.println(obj.isSubset(a1, a2, n, m));
+
         }
-	}
+    }
 }
 
 // } Driver Code Ends
 
-
-//User function Template for Java
-
+// User function Template for Java
 
 class Compute {
-    public String isSubset( long a1[], long a2[], long n, long m) {
-        
-        
+    public String isSubset(long a1[], long a2[], long n, long m) {
+
         Arrays.sort(a1);
         Arrays.sort(a2);
-        int j=0;
-        for(int i=0; i<n; i++)
-        {
-            if(j<m && a1[i]==a2[j])
-            {
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (j < m && a1[i] == a2[j]) {
                 j++;
             }
         }
-        if(j==m)
-        return "Yes";
+        if (j == m)
+            return "Yes";
         else
-        return "No";
+            return "No";
     }
 }
